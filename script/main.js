a029aa1f3fcb173370cf234554c64d451249e8b2
SaWeR <valera.sawer@yandex.ru>
2023-08-21 22:41:33 +0300

    22:41

--- script/main.js
+++ script/main.js
@@ -2,15 +2,14 @@ let back = document.getElementById('previous');
 let next = document.getElementById('slide-right');
 let slider = document.getElementsByClassName('gallery-slider');
 
+
 const imgElements = document.getElementsByClassName('slider-resources');
 const imgArr = Array.from(imgElements);
 
-function changeSlide() {
-    for (let i = 0; i < imgArr.length; i++) {
-        slider.remove(imgArr[i])
-    }
-};
+next.onclick = function changeSlideRight() {
+    slider.classList.toggle('transform: translateX(-100%)')
+}
 
-next.onclick = changeSlide;
 
 console.log(imgArr)
+
