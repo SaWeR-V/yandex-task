7066220a5f341cd8ecd4c23ff45f8722b292e260
SaWeR <valera.sawer@yandex.ru>
2023-08-22 11:48:52 +0300

    22.08.2023 11:49

--- script/main.js
+++ script/main.js
@@ -1,15 +1,83 @@
 let back = document.getElementById('previous');
-let next = document.getElementById('slide-right');
-let slider = document.getElementsByClassName('gallery-slider');
+let next = document.getElementById('next');
+let slideOne = document.getElementById('first');
+let slideTwo = document.getElementById('second');
+let slideThree = document.getElementById('third');
+let slideFour = document.getElementById('fourth');
 
+let currentImage = 0;
 
-const imgElements = document.getElementsByClassName('slider-resources');
-const imgArr = Array.from(imgElements);
+function checkForEnd() {
+    if (currentImage === 0) {
+        back.style.opacity = ('0.5');
+        back.disabled = (true);
+    }
+    else {
+        back.style.opacity = null;
+        back.disabled = (false);
+    }
 
-next.onclick = function changeSlideRight() {
-    slider.classList.toggle('transform: translateX(-100%)')
+    if (currentImage === 3){
+        next.style.opacity = ('0.5');
+        next.disabled = (true);
+    }
+    else {
+        next.style.opacity = (null);
+        next.disabled = (false);
+    }
 }
 
+checkForEnd();
 
-console.log(imgArr)
+next.onclick = function changeSlideRight() {
+    
+    if (currentImage === 0) {
+        slideOne.style.transform = (`translateX(-100%)`)
+        slideTwo.style.transform = (`translateX(-100%)`)
+        currentImage++;
+        checkForEnd();
+        return;
+    }
+    else if (currentImage === 1){
+        slideTwo.style.transform = (`translateX(-200%)`)
+        slideThree.style.transform = (`translateX(-200%)`)
+        currentImage++; 
+        checkForEnd();
+        return;
+    }
+    else if (currentImage === 2){
+        slideThree.style.transform = (`translateX(-300%)`)
+        slideFour.style.transform = (`translateX(-300%)`)
+        currentImage++; 
+        checkForEnd();
+        return;
+    }
+}
+
+back.onclick = function changeSlideLeft() {
+
+
+    if (currentImage === 1){
+        slideOne.style.transform = (`translateX(0%)`)
+        slideTwo.style.transform = (`translateX(0%)`)
+        currentImage--; 
+        checkForEnd();
+        return;
+    }
+    else if (currentImage === 2){
+        slideTwo.style.transform = (`translateX(-100%)`)
+        slideThree.style.transform = (`translateX(-100%)`)
+        currentImage--; 
+        checkForEnd();
+        return;
+    }
+    else if (currentImage === 3){
+        slideThree.style.transform = (`translateX(-200%)`)
+        slideFour.style.transform = (`translateX(-200%)`)
+        currentImage--; 
+        checkForEnd();
+        return;
+    }
+}
+
 
